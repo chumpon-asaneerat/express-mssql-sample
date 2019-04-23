e548081e9c28f745581b75b083f92a4814682ced
chumpon-asaneerat <chumponsenate@gmail.com>
2019-04-23 16:17:10 +0700

    Implement async mssql class wrapper with sample.

---
+++ NMSSql.js
@@ -0,0 +1,28 @@
+const mssql = require('mssql');
+
+class NMSSql {
+    constructor() {
+        this._conn = null;
+    }
+
+    async connect(opts) {
+        this._conn = new mssql.ConnectionPool(opts);
+        return await this._conn.connect(opts); // No callback so return promise.
+    }
+
+    async query(sqlText) {
+        let result = { 
+            data: {},
+            err: null
+        }
+        let req = new mssql.Request(this._conn);
+        return await req.query(sqlText); // No callback so return promise.
+    }
+
+    async disconnect() {
+        if (this._conn) await this._conn.close();
+        this._conn = null;
+    }
+};
+
+exports.NMSSql = module.exports.NMSSql = NMSSql;
--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const bodyparser = require('body-parser');
-const cookieparser = require('cookie-parser');
-const favicon = require('serve-favicon');
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const bodyparser = require("body-parser");
+const cookieparser = require("cookie-parser");
+const favicon = require("serve-favicon");
 
-const uuidv1 = require('uuid/v1');
+const uuidv1 = require("uuid/v1");
 
-const APPNAME = 'Express MsSQL Sample';
+const APPNAME = "Express MsSQL Sample";
 const PORT = 3000;
 const app = express();
 
 app.use(helmet());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.use(cookieparser('winnt@123'));
+app.use(cookieparser("winnt@123"));
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
-const iconpath = path.join(__dirname, 'public', 'favicon.ico');
+const iconpath = path.join(__dirname, "public", "favicon.ico");
 app.use(favicon(iconpath));
 
 function checkClientCookie(req, res, next) {
@@ -28,21 +28,58 @@ function checkClientCookie(req, res, next) {
     if (!client) {
         client = { id: uuidv1() };
         let opt = {
-            signed: true, 
-            httpOnly: true, 
+            signed: true,
+            httpOnly: true,
             maxAge: 1000 * 60 * 60 * 24 * 356 * 10 // maxAge = 10 years.
         };
-        res.cookie('client', client, opt);
+        res.cookie("client", client, opt);
     }
     res.locals.client = client;
     next();
-};
+}
 
 app.use(checkClientCookie);
 
-app.get('/', (req, res) => {
-    let { id } = res.locals.client;
-    res.send(`It's work!!. Client id: ${id}`);
+const nsql = require("./NMSSql");
+const db = new nsql.NMSSql();
+const opts = {
+    server: 'localhost',
+    database: 'TestDb7x3',
+    user: 'sa',
+    password: 'winnt123',
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    }
+}
+
+async function getSimpleQueryBase() {
+    await db.connect(opts);
+    let result = await db.query('SELECT * FROM CUSTOMER');
+    await db.disconnect();    
+    return result;
+}
+
+app.get("/", (req, res) => {
+    let dbResult = { records: [], errors: { errNum: 0, errMsg: '' } };
+    (async () => {
+        try {
+            let ret = await getSimpleQueryBase(); // try catch here.
+            dbResult.records = ret.recordsets[0];
+        }
+        catch (err) {
+            dbResult.errors.errNum = 101;
+            dbResult.errors.errMsg = err.message;
+        }
+    
+        let result = {
+            id: res.locals.client.id,
+            data: dbResult,
+            msg: "It's work!!."
+        };
+        res.json(result);
+    })()
 });
 
 const server = app.listen(PORT, () => {
