0850f5865589db0fc3bef6b85f8c2041c0188771
chumpon-asaneerat <chumponsenate@gmail.com>
2019-05-09 17:11:40 +0700

    Add adhoc method (test) and test code.

--- NMSSql.js
+++ NMSSql.js
@@ -53,6 +53,57 @@ class NMSSqlServer {
         req = new mssql.Request(this._conn);
         return req;
     };
+    
+    async adhoc(queryText, pObj, params) {
+        let result = { data: {}, errors: { errNum: -1, errMsg: 'No connection' }};
+        if (!this._conn) return result;
+        try {
+            // use prepare-unprepare.
+            let ps = new mssql.PreparedStatement(this._conn); // create PreparedStatement Object.
+            params.forEach(p => ps.input(p.name, p.sqltype)); // extract parameter(s).
+            await ps.prepare(queryText);
+            let ret = await ps.execute(pObj);
+            await ps.unprepare();
+            result = { data: ret, errors: { errNum: 0, errMsg: '' }};
+        }
+        catch (err) {
+            console.error('query error.', err);
+            result = { data: {}, errors: { errNum: 1, errMsg: err.message }};
+        }
+        return result;
+    }
 };
 
 exports.NMSSqlServer = module.exports.NMSSqlServer = NMSSqlServer;
+
+/*
+*** SAMPLE CASE: Use Array for generate parameters for SQL IN Clause
+
+var skus = ['product1', 'product2', 'product3'];
+
+// the statement
+SELECT *
+FROM stock AS s
+WHERE s.sku IN (@skus)
+
+var connection = new sql.Connection(config, function(err) {
+        var ps = new sql.PreparedStatement(connection);
+        // Construct an object of parameters, using arbitrary keys
+        var paramsObj = params.reduce((obj, val, idx) => {
+            obj[`id${idx}`] = val;
+            ps.input(`id${idx}`, sql.VarChar(200));
+            return obj;
+        }, {});
+        // Manually insert the params' arbitrary keys into the statement
+        var stmt = 'select * from table where id in (' + Object.keys(paramsObj).map((o) => {return '@'+o}).join(',') + ')';
+        ps.prepare(stmt, function(err) {
+            ps.execute(paramsObj, function(err, data) {
+                callback(null, data);
+                ps.unprepare(function(err) {
+                });
+            });
+        });
+    });
+}
+
+*/
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mssql = require('mssql');
+
 const nsql = require("./NMSSql");
 const opts = {
     server: 'localhost',
@@ -16,11 +18,19 @@ console.log('server.js running...');
 (async() => {
     let dbconn = new nsql.NMSSqlServer();
     await dbconn.connect(opts);
-    // do some request to database.
-    let req = dbconn.request();
-    if (req) {
-        console.log('request created.');
-    }
+    // do some adhoc query.
+    let params = [ 
+        { name:'param1', sqltype: mssql.Int },
+        { name:'param2', sqltype: mssql.Int } 
+    ];
+    let val = { param1: 123, param2: 456 };
+    let ret = await dbconn.adhoc('select @param1 as value; select @param2 as value2', val, params);
+    //console.log('ret:', ret.data);
+    console.log('recordsets:', ret.data.recordsets);
+    console.log('recordset:', ret.data.recordset);
+    console.log('output:', ret.data.output);
+    console.log('rowAffected:', ret.data.rowAffected);
+    console.log('returnValue:', ret.data.returnValue);
     // disconnect it.
     await dbconn.disconnect();
 })();
