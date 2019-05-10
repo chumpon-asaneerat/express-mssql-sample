16bd2a264fdb938fed304501d5fa014fb403a716
chumpon-asaneerat <chumponsenate@gmail.com>
2019-05-10 10:17:10 +0700

    Add prefer usage (for re-design later).

--- NSql2.js
+++ NSql2.js
@@ -8,3 +8,14 @@ class NSqlType {
 }
 
 module.exports.NSqlType = exports.NSqlType = NSqlType;
+
+/*
+let db = {
+    query: function(str, params) {}
+};
+let params = [
+    { name: 'param1', type:'Int', value: 123 },
+    { name: 'param2', type:'nvarchar(50)', value: ['A', 'C', 'D'] }
+]
+db.query('select @param1 as const, id from table1 where item in @param2', params)
+*/
