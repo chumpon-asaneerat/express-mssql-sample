565df06a5d10eed951201c8494f3c8a4964a55da
chumpon-asaneerat <chumponsenate@gmail.com>
2019-05-11 19:57:37 +0700

    servet.js add data access class design topic.

--- server.js
+++ server.js
@@ -4,3 +4,26 @@
 // functional due to append difference mixin functions.
 
 // mixin 3: figure out how to access class static method.
+
+// database classes:
+// 1. abstract classes :
+//    - dbaccess class (should inherited for each provider)
+//    - dbtypes class (should inherited for each provider)
+//      for example :
+//      dbtypes { } // no implements in base class (may be not need this class)
+//      sqltypes extends dbtypes {
+//          static get char(size = 50) { return new sqltype(`char('${size}')`) }
+//          static get nchar(size = 50) { return new sqltype(`nchar('${size}')`) }
+//          static get int() { return new sqltype(`int`) }
+//      }
+//
+//      - dbtype (should inherited for each provider)
+//      for example :
+//      dbtype { } // no implements in base class (may be not need this class)
+//      sqltype extends dbtype {
+//          constructor(str) {
+//              this._type = ... [keep type in string lower case]
+//              this._p1 = ... [keep first param: size, length, precision]
+//              this._p2 = ... [keep second param: scale]
+//          }
+//      }
