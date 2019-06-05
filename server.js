f8436d49433c2a4c48b3559186fa06ea6ddcb4e2
chumpon-asaneerat <chumponsenate@gmail.com>
2019-06-05 13:03:54 +0700

    nlib.core.js add clone and setValues with sample.

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 const nlib = require('./src/server/nlib/nlib.core');
 
+// Test Object Manipulations (clone and setValues).
+/*
+let src1 = { No: 1, sVal: 'my data', ToDay: new Date() }
+let dst1 = nlib.clone(src1, true); // case sensitive.
+let dst2 = nlib.clone(src1, false); // ignore case.
+console.log(dst1);
+console.log(dst2);
+
+let dst3 = { No: 10, sVal: 'my another data', ToDay: null }
+let dst4 = { No: 10, sVal: 'my another data', ToDay: null }
+let src2 = { sval: 'my information', today: new Date('2019-11-30') }
+// The No property not exist in src obj so null value is assigned.
+nlib.setValues(dst3, src2, true);
+console.log(dst3);
+// The No property not exist in src obj so exist value is not changed.
+nlib.setValues(dst4, src2, false);
+console.log(dst4);
+*/
+
+// Test DateTime
+/*
 const DateTime = require('./src/server/nlib/nlib.datetime').DateTime;
 console.log('static today:', DateTime.today);
 let dt = new DateTime();
 console.log('instance today:', dt.today);
+*/
 
+// Test Database Connection
+/*
 const DbConnection = require('./src/server/nlib/nlib.dbconnection');
 
 let fn = (async () => {
@@ -29,16 +53,18 @@ let ex1 = (async () => {
 
 ex1();
 
-/*
 // Run all in sync.
-(async () => {
-    await fn();
-    await ex1();
-})();
+// (async () => {
+//     await fn();
+//     await ex1();
+// })();
 */
 
+// Test Paths constants and functions.
+/*
 const paths = require('./src/server/nlib/nlib.paths');
 console.log('root:', paths.root);
+*/
 
 /*
 The connection config should look like this
--- src/server/nlib/nlib.core.js
+++ src/server/nlib/nlib.core.js
@@ -1,8 +1,29 @@
 /** The Nlib library. */
 const nlib = {
     // common functions.
-    /** The log function. */
-    log: (message, ...optionalParams) => { console.log(message, ...optionalParams); }
+    //? =============================
+    //? Object manipulation methods.
+    //? =============================
+    /** Create New object with clone all properties with supports ignore case sensitive. */
+    clone: (o, caseSensitive) => {
+        let result = {}
+        let ignoreCase = (caseSensitive) ? false : true;
+        let keys = Object.keys(o);
+        keys.forEach((key) => { result[(ignoreCase) ? key.toLowerCase() : key] = o[key]; });
+        return result;
+    },
+    /** 
+     * Set dest object's properties that match src object's property with case insensitive.
+     * If dest property not exist in src obj and overwrite flag is set so null value is assigned
+     * otherwise if overwrite flag is not set the original dest property will not changed.
+     * */
+    setValues: (dest, src, overwrite) => {
+        let keys = Object.keys(dest);
+        keys.forEach(key => {
+            let dKey = key.toLowerCase();
+            dest[key] = (!src[dKey]) ? (!overwrite) ? dest[key] : null : src[dKey];
+        });
+    }
 }
 
 /** The Nlib library. */
